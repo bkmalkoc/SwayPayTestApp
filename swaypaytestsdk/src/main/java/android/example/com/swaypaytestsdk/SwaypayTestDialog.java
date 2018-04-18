775b7f97f3c7983be2380f1da82bdd2fdf676f73
Burak Malkoc <malkoc.burak@yahoo.com>
2018-04-18 10:10:51 -0700

    Initial commit

---
+++ swaypaytestsdk/src/main/java/android/example/com/swaypaytestsdk/SwaypayTestDialog.java
@@ -0,0 +1,26 @@
+package android.example.com.swaypaytestsdk;
+
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SwaypayTestDialog extends DialogFragment {
+
+    public SwaypayTestDialog() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog, container);
+    }
+
+    @Override
+    public void show(FragmentManager manager, String tag) {
+        super.show(manager, tag);
+    }
+}
